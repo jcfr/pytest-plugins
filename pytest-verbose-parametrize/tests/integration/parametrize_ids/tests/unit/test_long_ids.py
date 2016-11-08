81e0b058ab757ea102134158f9b76e490ad25a45
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-11-08 18:41:09 -0500

    Relocate pytest-virtualenv to allow install from requirements.txt
    This commit was automatically created by https://gist.github.com/jcfr/19a360f541b5caae5f31ac31c83e4f65
    
    It is based of:
    * repository: git@github.com:jcfr/pytest-plugins.git
    *     branch: pytest-virtualenv_install-package_support-windows
    *     commit: c0dd757
    *       date: 2016-11-08

--- pytest-verbose-parametrize/tests/integration/parametrize_ids/tests/unit/test_long_ids.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-@pytest.mark.parametrize(('x', 'y', ), [(list(range(100)), None), ])
-def test_foo(x, y):
-    assert y not in x
--- pytest-webdriver/tests/unit/test_webdriver.py
+++
@@ -1,2 +0,0 @@
-def test_import():
-    import pytest_webdriver
